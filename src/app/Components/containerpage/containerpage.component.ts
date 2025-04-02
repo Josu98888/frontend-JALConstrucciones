69f3d97aaadf8f2af6151c06da451e7890dca2fa
Josué Aquino <aquinojosue889@gmail.com>
2025-04-02 17:03:15 -0300

    Se créó el componente containerPage para dejar el lugar correcto a router-oulet

---
+++ src/app/Components/containerpage/containerpage.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-containerpage',
+  imports: [],
+  templateUrl: './containerpage.component.html',
+  styleUrl: './containerpage.component.css'
+})
+export class ContainerpageComponent {
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './Components/header/header.component';
 import { FooterComponent } from './Components/footer/footer.component';
+import { ContainerpageComponent } from './Components/containerpage/containerpage.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, ContainerpageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
