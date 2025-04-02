891efa474968a8c6a0f266ecfeaa264df54a014c
Josué Aquino <aquinojosue889@gmail.com>
2025-04-02 18:26:11 -0300

    Se creó la pagina aboutUs con su respectiva ruta

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './Pages/home/home.component';
+import { AboutUsComponent } from './Pages/about-us/about-us.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
+    {path: 'aboutUs', component: AboutUsComponent}
 ];
