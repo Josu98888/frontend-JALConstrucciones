afbb068cddc34761955f5486b9a6a064d3d15fae
Josué Aquino <aquinojosue889@gmail.com>
2025-04-02 19:37:58 -0300

    Se creó el componente contactUs y se le añadio una ruta para luego utilizar como page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './Pages/home/home.component';
 import { AboutUsComponent } from './Pages/about-us/about-us.component';
+import { ContactUsComponent } from './Components/contact-us/contact-us.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
-    {path: 'aboutUs', component: AboutUsComponent}
+    {path: 'aboutUs', component: AboutUsComponent},
+    {path: 'contactUs', component: ContactUsComponent}
 ];
