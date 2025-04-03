f327c0f956a072a7e2e10e6a11c5c85e1c5e9c5d
Josué Aquino <aquinojosue889@gmail.com>
2025-04-02 22:08:13 -0300

    Se creó el categoryService con su metódo getCategorias y se utilizó en el componente header donde se añadio la lista de categorias

--- src/app/Components/header/header.component.ts
+++ src/app/Components/header/header.component.ts
@@ -1,17 +1,47 @@
-import { NgClass, NgIf } from '@angular/common';
+import { CommonModule, NgClass, NgFor } from '@angular/common';                                 //para agregar o quitar clases CSS dinámicamente en un elemento HTML según condiciones en el componente.
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink } from '@angular/router';                              //para  navegar entre rutas sin recargar la página.
+import { CategoryService } from '../../services/categoryService';           // Servicio de la categoria
 
 @Component({
   selector: 'app-header',
-  imports: [NgClass, RouterLink],
+  imports: [NgClass, RouterLink, NgFor, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
+
 export class HeaderComponent {
+  public categories:any;
+  public status!:string;
   public menuItems:boolean = false;
+  public menuCategories:boolean = false;
+
+  constructor(
+    private _categoryService:CategoryService
+  ) {
+    this.categories = this.getCategories();
+  }
 
   togleMenu() {
     this.menuItems = !this.menuItems;
+  }
+
+  togleCategories() {
+    this.menuCategories = !this.menuCategories;
+  }
+
+  getCategories() {
+    this._categoryService.getCategories().subscribe(
+      response => {
+        if(response.status == 'success') {
+          this.categories = response.categories;
+          this.status = 'success';
+        }
+      },
+      error => {
+        console.log(error);
+        this.status = 'error';
+      }
+    );
   }
 }
---
+++ src/app/services/categoryService.ts
@@ -0,0 +1,24 @@
+import { Injectable } from "@angular/core";                                                   //para definir servicios que pueden ser inyectados en otros componentes o servicios.
+import { HttpClient, HttpHeaders } from "@angular/common/http";                               //para hacer peticiones HTTP
+import { Observable } from "rxjs";                                                            // Para recibir el Observable cuando se hace una petición HTTP  con HttpClient
+import { environment } from "../../environments/environment.development";
+
+@Injectable({
+    providedIn: 'root',
+})                                                                                 //decorador pára no instaciar la clase donde se la cree
+export class CategoryService {
+    public url:string;
+
+    constructor(
+        public _http: HttpClient
+    ) {
+        this.url = environment.url;
+    }
+
+    getCategories(): Observable<any> {
+        let headers = new HttpHeaders();
+
+        return this._http.get(this.url + 'category', {headers: headers});
+    }
+}
+
