3b19e374b56d52c36a93a44c4b04a1b52a7f5457
Josué Aquino <aquinojosue889@gmail.com>
2025-04-11 13:58:15 -0300

    Se creó el componente whichoose con diseño para mobile y se añadio a la pagina Home

---
+++ src/app/Components/whychooseus/whychooseus.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-whychooseus',
+  imports: [],
+  templateUrl: './whychooseus.component.html',
+  styleUrl: './whychooseus.component.css'
+})
+export class WhychooseusComponent {
+
+}
--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { OutstandingComponent } from '../../Components/outstanding/outstanding.component';
 import { ShowcategoriesComponent } from '../../Components/showcategories/showcategories.component';
+import { WhychooseusComponent } from '../../Components/whychooseus/whychooseus.component';
 
 @Component({
   selector: 'app-home',
-  imports: [OutstandingComponent, ShowcategoriesComponent],
+  imports: [OutstandingComponent, ShowcategoriesComponent, WhychooseusComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
