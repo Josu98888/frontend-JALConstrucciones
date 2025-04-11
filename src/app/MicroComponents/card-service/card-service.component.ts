f2f949fbcd2df362e893292474e9b3483cf20dd0
Josué Aquino <aquinojosue889@gmail.com>
2025-04-11 12:03:53 -0300

    Se añadio a cardService la descrpcion opcional

--- src/app/MicroComponents/card-service/card-service.component.ts
+++ src/app/MicroComponents/card-service/card-service.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { Service } from '../../Models/service';
 import { Image } from '../../Models/image';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-card-service',
-  imports: [],
+  imports: [NgIf],
   templateUrl: './card-service.component.html',
   styleUrl: './card-service.component.css'
 })
@@ -12,4 +13,5 @@ export class CardServiceComponent {
   @Input() service:any;
   @Input() image:any;
   @Input() url!:string;
+  @Input() description:boolean = false;
 }
