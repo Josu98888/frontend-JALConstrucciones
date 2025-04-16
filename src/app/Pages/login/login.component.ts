087df1919b349fae43ce94222b61c7c38887437e
Josué Aquino <aquinojosue889@gmail.com>
2025-04-15 23:09:34 -0300

    Se creó el link en el footer hacia la page login creada con su diseño para mobile

---
+++ src/app/Pages/login/login.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { TitleComponent } from '../../MicroComponents/title/title.component';
+
+@Component({
+  selector: 'app-login',
+  imports: [TitleComponent],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './Pages/home/home.component';
 import { AboutUsComponent } from './Pages/about-us/about-us.component';
 import { ServicesbycategoryComponent } from './Pages/servicesbycategory/servicesbycategory.component';
 import { ServiceDetailComponent } from './Pages/service-detail/service-detail.component';
+import { LoginComponent } from './Pages/login/login.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'services/:id', component: ServicesbycategoryComponent},
     {path: 'aboutUs', component: AboutUsComponent},
-    {path: 'serviceDetail/:id', component: ServiceDetailComponent}
+    {path: 'serviceDetail/:id', component: ServiceDetailComponent},
+    {path: 'login', component: LoginComponent}
 ];
