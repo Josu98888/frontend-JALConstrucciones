fd5853cbb6776685a4e465e1d8cea4cf611d4aa7
Josué Aquino <aquinojosue889@gmail.com>
2025-04-17 18:45:59 -0300

    Se hizó un refactor en el component createCategory para usarlo en la edicion de categoria que fue añadido en la page Categories con diseño para mobile

--- src/app/Components/create-category/create-category.component.ts
+++ src/app/Components/create-category/create-category.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { NgFor, NgIf } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-create-category',
-  imports: [],
+  imports: [NgIf],
   templateUrl: './create-category.component.html',
   styleUrl: './create-category.component.css'
 })
 export class CreateCategoryComponent {
+  @Input() title:string = 'Creá una categoría';
+  @Input() edit:boolean = false;
+  @Input() categories:any;
+  @Input() image:any;
+  @Input() imagePreview:any;
+  @Input() url:any;
+  @Input() onSubmit!: () => void;
 
+  
 }
