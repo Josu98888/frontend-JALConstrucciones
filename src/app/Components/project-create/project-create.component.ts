12c673907623823000e1f7cf70e51123c354b3aa
Josué Aquino <aquinojosue889@gmail.com>
2025-04-24 01:34:07 -0300

    Se creó el metódo avatarUpload en el componente projectCreate para la subida de imagenes con vista previa

--- src/app/Components/project-create/project-create.component.ts
+++ src/app/Components/project-create/project-create.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { NgFor } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-project-create',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, NgFor],
   templateUrl: './project-create.component.html',
-  styleUrl: './project-create.component.css'
+  styleUrl: './project-create.component.css',
 })
 export class ProjectCreateComponent {
+  @Output() formProject = new EventEmitter<any[]>();
+  @Input() category_id: any;
+  public form: any;
+  public fileAvatars: File[] = [];
+  public imagePreviews: (string | ArrayBuffer | null)[] = [];
 
+  avatarUpload($event: any) {
+    const files = Array.from($event.target.files).slice(0, 3) as File[]; // hasta 3
+    this.fileAvatars = files;
+    this.imagePreviews = [];
+
+    files.forEach((file) => {
+      const reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onloadend = () => {
+        this.imagePreviews.push(reader.result);
+      };
+    });
+
+    if ($event.target.files.length > 3) {
+      alert('Solo se procesarán las primeras 3 imágenes.');
+    }
+  }
 }
