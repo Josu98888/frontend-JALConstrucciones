927f0e10d406222208eccb9319d5c07d9715d97b
Josué Aquino <aquinojosue889@gmail.com>
2025-04-24 14:15:35 -0300

    Se creó el metódo getCategories en el componente projectCreate.ts con el uso de categoryService y se lo añadió al form del projectCreate.html

--- src/app/Components/project-create/project-create.component.ts
+++ src/app/Components/project-create/project-create.component.ts
@@ -6,6 +6,7 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
+import { CategoryService } from '../../services/categoryService';
 
 @Component({
   selector: 'app-project-create',
@@ -13,15 +14,23 @@ import {
   templateUrl: './project-create.component.html',
   styleUrl: './project-create.component.css',
 })
+
 export class ProjectCreateComponent {
   @Output() formProject = new EventEmitter<any[]>();
-  public category_id: any = 1;
+  public categories:any;
   public form: any;
   public fileAvatars: File[] = [];
   public imagePreviews: (string | ArrayBuffer | null)[] = [];
 
+  constructor(
+    private _categoryService: CategoryService
+  ) {
+
+  }
   ngOnInit() {
+    this.getCategories();
     this.form = new FormGroup({
+      category_id: new FormControl('', Validators.required),
       name: new FormControl('', Validators.required),
       description: new FormControl('', Validators.required),
       outstanding: new FormControl('', Validators.required),
@@ -35,10 +44,10 @@ export class ProjectCreateComponent {
         formData.append('image', file);
       });
     }
-    // console.log(this.form.value);
-    // for (const [key, value] of formData.entries()) {
-    //   console.log(key, value);
-    // }
+    console.log(this.form.value);
+    for (const [key, value] of formData.entries()) {
+      console.log(key, value);
+    }
     this.formProject.emit([this.form, formData]);
   }
 
@@ -58,5 +67,18 @@ export class ProjectCreateComponent {
     if ($event.target.files.length > 3) {
       alert('Solo se procesarán las primeras 3 imágenes.');
     }
+  }
+
+  getCategories() {
+    this._categoryService.getCategories().subscribe(
+      response => {
+        if(response.status == 'success') {
+          this.categories = response.categories;
+        }
+      },
+      error => {
+        console.log(error);
+      }
+    );
   }
 }
