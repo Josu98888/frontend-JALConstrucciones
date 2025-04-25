f349879f1160ab9ef2ea46ac87b517d96378c708
Josué Aquino <aquinojosue889@gmail.com>
2025-04-25 01:06:31 -0300

    Se creó el metódo delete en el microComponent cardDetail.ts

--- src/app/MicroComponents/card-detail/card-detail.component.ts
+++ src/app/MicroComponents/card-detail/card-detail.component.ts
@@ -3,6 +3,8 @@ import { MysliderComponent } from '../myslider/myslider.component';
 import { NgFor, NgIf } from '@angular/common';
 import { TitleComponent } from '../title/title.component';
 import { UserService } from '../../services/userService';
+import { Service } from '../../services/serviceService';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-card-detail',
@@ -15,14 +17,30 @@ export class CardDetailComponent {
   @Input() public images:any;
   @Input() public url!:string;
   public identity:any;
+  public token:string;
+  public status:string = '';
 
   constructor(
-    private _userService:UserService
+    private _userService:UserService,
+    private _serviceService:Service,
+    private _router:Router
   ) {
-    this.identity = _userService.getIdentity();
+    this.identity = this._userService.getIdentity();
+    this.token = this._userService.getToken();
   }
 
   delete() {
-    
+    this._serviceService.deleteService(this.token, this.service.id).subscribe(
+      response => {
+        if(response.status == 'success') {
+          this.status = 'success';
+          this._router.navigate(['']);
+        }
+      },
+      error => {
+        console.log(error);
+        this.status = 'error';
+      }
+    );
   }
 }
