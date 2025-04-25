8b1d7aa919ae0874eb72946fe22f48a3f0e27641
Josué Aquino <aquinojosue889@gmail.com>
2025-04-25 00:49:36 -0300

    Se creó el btn en el microComponent cardDetail para eliminar, solo el diseño en mobile sin funcionalidad

--- src/app/MicroComponents/card-detail/card-detail.component.ts
+++ src/app/MicroComponents/card-detail/card-detail.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { MysliderComponent } from '../myslider/myslider.component';
 import { NgFor, NgIf } from '@angular/common';
 import { TitleComponent } from '../title/title.component';
+import { UserService } from '../../services/userService';
 
 @Component({
   selector: 'app-card-detail',
@@ -13,5 +14,15 @@ export class CardDetailComponent {
   @Input() public service:any;
   @Input() public images:any;
   @Input() public url!:string;
+  public identity:any;
 
+  constructor(
+    private _userService:UserService
+  ) {
+    this.identity = _userService.getIdentity();
+  }
+
+  delete() {
+    
+  }
 }
