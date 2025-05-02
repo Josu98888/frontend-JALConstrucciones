9a42957cb55fc1bb5d5e95777f0d75a4626b4836
Josué Aquino <aquinojosue889@gmail.com>
2025-05-01 21:19:41 -0300

    Se creó el diseño responsivo en laptop en la Page Home

--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -4,10 +4,11 @@ import { ShowcategoriesComponent } from '../../Components/showcategories/showcat
 import { WhychooseusComponent } from '../../Components/whychooseus/whychooseus.component';
 import { ContactUsComponent } from '../../Components/contact-us/contact-us.component';
 import { SocialNetworksComponent } from '../../Components/social-networks/social-networks.component';
+import { TitleComponent } from '../../MicroComponents/title/title.component';
 
 @Component({
   selector: 'app-home',
-  imports: [OutstandingComponent, ShowcategoriesComponent, WhychooseusComponent, ContactUsComponent, SocialNetworksComponent],
+  imports: [OutstandingComponent, ShowcategoriesComponent, WhychooseusComponent, ContactUsComponent, SocialNetworksComponent, TitleComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
